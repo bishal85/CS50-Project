e0922dfbbfbce4ad866e029fbec61a40e4d0c7dc
bishal85 <143246886+bishal85@users.noreply.github.com>
2023-12-10 10:16:50 +0000

    Sun, Dec 10, 2023, 4:16 PM +06:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,17 +1,21 @@
+
 string=input("Expression: ")
 j=0
 b=string.find('+')
 c=string.find('-')
 d=string.find('*')
 e=string.find('/')
-if b==1:
+string=string.replace(" ","")
+if b!=0:
+   
     string1=string.replace('+','')
     string1=int(string1.strip())
     while string1>0:
         a=int(string1%10)
         j=float(j+a)
         string1=int(string1/10)
-elif c==1:
+elif c!=0:
+   
     string1=string.replace('-','')
     string1=int(string1.strip())
     while string1>0:
@@ -20,7 +24,8 @@ elif c==1:
          string1=int(string1/10)
          if string1==0:
              break
-elif d==1:
+elif d!=0:
+   
     string1=string.replace('*','')
     j=1
     string1=int(string1.strip())
@@ -29,7 +34,7 @@ elif d==1:
          a=int(string1%10)
          j=float(j*a)
          string1=int(string1/10)
-elif e==1:
+elif e!=0:
     string1=string.replace('/','')
     j=1
     string1=int(string1.strip())
