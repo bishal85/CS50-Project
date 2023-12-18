a76b3247196ff430035abff1c3ca0566590506a5
bishal85 <143246886+bishal85@users.noreply.github.com>
2023-12-18 11:44:09 +0000

    Mon, Dec 18, 2023, 5:44 PM +06:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,10 +1,10 @@
 import emoji
 string=input("input ")
 
-c=string.find(", ")
+c=string.find(",")
 if c!=0:
-    a,b=string.split(",")
-    print("Output: "+a+emoji.emojize(b))
+    a,b=string.split(", ")
+    print("Output: "+a+' '+emoji.emojize(b))
 else:
 
      print("Output: "+emoji.emojize(string))
