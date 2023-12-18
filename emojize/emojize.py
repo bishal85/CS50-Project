3f3f2e57f2523d52bb02319e2188c3e61cc381ad
bishal85 <143246886+bishal85@users.noreply.github.com>
2023-12-18 12:04:24 +0000

    Mon, Dec 18, 2023, 6:04 PM +06:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -3,7 +3,7 @@ string=input("input ")
 
 c=string.find(", ")
 if c!=-1:
-    a,b=string.split(", ")
+    a,b=string.split(" ")
     print("Output: "+a+' '+emoji.emojize(b, language='alias'))
 else:
     print("Output: "+emoji.emojize(string, language='alias'))
