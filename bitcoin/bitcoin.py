7dbd314c4a8d915703dd8fe5735bdc5f810f131a
bishal85 <143246886+bishal85@users.noreply.github.com>
2023-12-19 10:31:33 +0000

    Tue, Dec 19, 2023, 4:31 PM +06:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,20 +4,26 @@ import sys
 
 try:
     c=0
+    e=0
+    string4=''
     string2=float(input())
     string=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     string1=string.json()
     a=str(float(string1["bpi"]["USD"]["rate_float"])*string2)
     m,n=a.split(".")
     while len(m)>0:
-
+          e=e+1
           c=c+1
           if c%3==0:
-               
+               string4=string4+m[e:e+1]+","
+               c=0
+          else:
+               string4=string4+m[e:e+1]
 
 
 
-    print(m)
+
+    print(string4)
 
 
 
