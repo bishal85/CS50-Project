d056d779897fd4547063d7a52ec4a4212982ee62
bishal85 <143246886+bishal85@users.noreply.github.com>
2023-12-19 11:00:43 +0000

    Tue, Dec 19, 2023, 5:00 PM +06:00

---
+++ adieu/adieu.py
@@ -0,0 +1,27 @@
+string2=''
+while True:
+    try:
+        string=input("Name: ")
+        string2=string2+string+" "
+    except EOFError:
+        string2=string2.strip()
+        break
+    else:
+        pass
+string1=''
+c=0
+m=0
+for b in string2:
+    if b==" ":
+        c=c+1
+for a in string2:
+    if a==" ":
+        m=m+1
+        if m!=c:
+             string1=string1+', '
+        elif m==c:
+             string1=string1+" and "
+             c=0
+    else:
+        string1=string1+a
+print("Adieu, adieu to "+string1)
