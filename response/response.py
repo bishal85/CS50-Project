806e16dbf5e22514ae5fc1adae41607ca4ac5f08
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-01 10:48:28 +0000

    Mon, Jan 1, 2024, 4:48 PM +06:00

--- response/response.py
+++ response/response.py
@@ -5,7 +5,7 @@ def main():
     print(string1(input("What's your email address? ")))
 def string1(string):
     if validators.email(string)==True:
-        return f"valid"
+        return f"Valid"
     else:
         return f"Invalid"
 if __name__=="__main__":
