fbd56a42ee22a762eb7ccfee0bf4dbd4f08babfb
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-03 12:57:12 +0000

    Wed, Jan 3, 2024, 6:57 PM +06:00

--- jar/jar.py
+++ jar/jar.py
@@ -4,7 +4,7 @@ class Jar:
     def __init__(self, capacity=12):
         self.line=line
         self.n=n
-  
+
 
     def deposit(n):
         n=int(n)
@@ -22,7 +22,12 @@ class Jar:
             a1.append([k])
         return a1
 def main():
-    jar=Jar.deposit(2)
-    print(jar)
+    a1=[]
+    file=open("hello.csv")
+    line=csv.DictReader(file)
+    for k in line:
+            a1.append([k])
+    print(a1)
+
 if __name__=="__main__":
     main()
