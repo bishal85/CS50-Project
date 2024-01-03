ccdcd0f7c9866b43fde82931bd2a88834fcb1bf0
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-03 12:52:23 +0000

    Wed, Jan 3, 2024, 6:52 PM +06:00

--- jar/jar.py
+++ jar/jar.py
@@ -4,21 +4,10 @@ class Jar:
     def __init__(self, capacity=12):
         self.line=line
         self.n=n
-
     def __str__(self):
-        a1=[]
-        file=open("hello.csv",'r')
-        line=file.readlines()
-        for k in line:
-            a1.append(k)
-        return a1
-
-
+       
     def deposit(n):
         m=int(n)
-
-
-
         j="a"
         csvfile=open("hello.csv",'w')
 
@@ -32,15 +21,8 @@ class Jar:
         for k in line:
             a1.append([k])
         return a1
-
-
-
 def main():
-
     jar=Jar.deposit(2)
     print(jar)
-
-
-
 if __name__=="__main__":
     main()
