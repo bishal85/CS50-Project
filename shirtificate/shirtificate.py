a37d5ce2282f1355bf99e82f74ceee78610bfac1
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 11:39:35 +0000

    Fri, Jan 5, 2024, 5:39 PM +06:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,7 +1,9 @@
 from fpdf import FPDF
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
-pdf.image('shirtificate.png', h=pdf.eph)
+pdf.image('shirtificate.png', 10,70,w=190)
+
 pdf.set_font("helvetica", "B", 16)
-pdf.cell(40, 10, "Hello World!")
-pdf.output("shirtificate2.pdf")
+pdf.cell(0,214,"I took cs50",align="C")
+
+pdf.output("he2.pdf")
