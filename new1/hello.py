4030a7a6cb3a3b10ab98b9407fdf4b16fc6704fb
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 11:58:57 +0000

    Fri, Jan 5, 2024, 5:58 PM +06:00

--- new1/hello.py
+++ new1/hello.py
@@ -7,5 +7,8 @@ pdf.set_font("helvetica","", 47)
 pdf.cell(0,57,"CS50 shirtificate",align="C")
 pdf.set_text_color(255,255,255)
 pdf.set_font("helvetica", "B", 24)
+pdf.cell(0,214,"I took CS50", align="C")
 
-pdf.output("hello2.pdf")
+
+
+pdf.output("hello4.pdf")
