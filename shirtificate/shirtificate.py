be3383738b6a70f7f20067cf1ba85151980ad2a2
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 12:13:39 +0000

    Fri, Jan 5, 2024, 6:13 PM +06:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,12 +8,17 @@ class PDF(FPDF):
         self.cell(0,57,"CS50 shirtificate",align="C")
         self.ln(20)
 
-def string1():
-    pdf = PDF(orientation="P", unit="mm", format="A4")
-    pdf.add_page()
-    pdf.image('shirtificate.png', 10,70,w=190)
+def main():
+    string=input(" ")
 
-    pdf.set_font("helvetica", "B", 16)
-    pdf.cell(0,214,"I took cs50",align="C")
+    string1(string)
+def string1(b):
+    pdf = PDF()
+    pdf.add_page(orientation="portrait", format="A4")
 
-    pdf.output("he2.pdf")
+
+    pdf.set_text_color(255,255,255)
+
+    pdf.set_font("helvetica", "B", 24)
+    pdf.cell(0,214,f"{b} took cs50",align="C")
+    pdf.output("string.pdf")
