77c92637ff9e11dac6eb7bae11ed2070d2952975
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 11:56:40 +0000

    Fri, Jan 5, 2024, 5:56 PM +06:00

--- new1/hello.py
+++ new1/hello.py
@@ -4,10 +4,8 @@ pdf.add_page()
 pdf.image('shirtificate.png', 10,70,w=190)
 pdf.set_font("helvetica","", 47)
 
-pdf.cell(0,214,"CS50 shirtificate",align="C")
+pdf.cell(0,57,"CS50 shirtificate",align="C")
 pdf.set_text_color(255,255,255)
 pdf.set_font("helvetica", "B", 24)
 
-pdf.cell(0,214,"I took cs50",align="C")
-
-pdf.output("new5.pdf")
+pdf.output("hello2.pdf")
