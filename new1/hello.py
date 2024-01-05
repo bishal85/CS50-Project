2bbbc7e844a99f7810d091a36d7832a6ecc90e75
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 12:01:20 +0000

    Fri, Jan 5, 2024, 6:01 PM +06:00

--- new1/hello.py
+++ new1/hello.py
@@ -2,13 +2,12 @@ from fpdf import FPDF
 pdf = FPDF(orientation="portrait", format="A4")
 pdf.add_page()
 pdf.image('shirtificate.png', 10,70,w=190)
-pdf.set_font("helvetica","", 47)
-
+pdf.set_font("helvetica","", 48)
 pdf.cell(0,57,"CS50 shirtificate",align="C")
 pdf.set_text_color(255,255,255)
 pdf.set_font("helvetica", "B", 24)
-pdf.cell(0,214,"I took CS50", align="C")
+pdf.cell(0,160,"I took CS50", align="C")
 
 
 
-pdf.output("hello4.pdf")
+pdf.output("hello7.pdf")
