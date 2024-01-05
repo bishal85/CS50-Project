a2c42d729bb9e7094afe0310f03a7579025946a7
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 11:46:14 +0000

    Fri, Jan 5, 2024, 5:46 PM +06:00

--- new1/hello.py
+++ new1/hello.py
@@ -3,8 +3,8 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.image('shirtificate.png', 10,70,w=190)
 pdf.set_text_color(255,255,255)
-pdf.set_font("helvetica", "B", 16)
+pdf.set_font("helvetica", "B", 24)
 
 pdf.cell(0,214,"I took cs50",align="C")
 
-pdf.output("he2.pdf")
+pdf.output("new2.pdf")
