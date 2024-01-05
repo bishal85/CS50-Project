10271f0da4462eef3091cb16aa4e8f6b1a840da4
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 12:22:38 +0000

    Fri, Jan 5, 2024, 6:22 PM +06:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,8 +2,7 @@ from fpdf import FPDF
 class PDF(FPDF):
 
     def Header(self):
-        self.add_page()
-        self.image('shirtificate.png', 10,70,w=190)
+        self.image('./shirtificate.png', 10,70,w=190)
         self.set_font("helvetica","", 48)
         self.cell(0,57,"CS50 shirtificate",align="C")
         self.ln(20)
@@ -15,12 +14,9 @@ def main():
 def string1(b):
     pdf = PDF()
     pdf.add_page(orientation="portrait", format="A4")
-
-
     pdf.set_text_color(255,255,255)
-
     pdf.set_font("helvetica", "B", 24)
     pdf.cell(0,214,f"{b} took cs50",align="C")
     pdf.output("string.pdf")
-if __name__="__main__":
+if __name__=="__main__":
     main()
