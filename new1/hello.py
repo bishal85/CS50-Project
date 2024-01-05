5b3892b4a5fbfea3abf6739d5969887d3d68c01e
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-05 12:39:58 +0000

    Fri, Jan 5, 2024, 6:39 PM +06:00

--- new1/hello.py
+++ new1/hello.py
@@ -11,14 +11,12 @@ def string1(b):
     pdf.image("./shirtificate.png", 10,70,190)
     pdf.set_font("helvetica","", 48)
     pdf.cell(0,57,"CS50 shirtificate",align="C")
-    pdf.ln(20)
-    pdf.add_page(orientation="portrait", format="a4")
     pdf.set_font("helvetica",size=24)
 
 
 
-    pdf.set_text_color(255,255,255)
+    pdf.set_text_color(0,0,255)
     pdf.cell(0,214,f"{b} took cs50",align="C")
-    pdf.output("string2.pdf")
+    pdf.output("b.pdf")
 if __name__=="__main__":
     main()
