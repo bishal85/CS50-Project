9f9879ee9f92f60bee21355e140b1b17e9df9a60
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 11:12:31 +0000

    Tue, Jan 9, 2024, 5:12 PM +06:00

---
+++ shirtificate/html.py
@@ -0,0 +1,38 @@
+from spire.presentation import Presentation as presentation
+from spire.doc import Document as document
+import sys
+import os
+
+def docxtopdf(a,b):
+   document.LoadFromFile(a)
+   document.SaveToFile(b, FileFormat.PDF)
+   document.Close()
+def pdftodocx(a,b):
+
+   print(f"{a}to{b}")
+
+
+def ptopdf(a,b):
+# Load a PowerPoint presentation in PPTX format
+   presentation.LoadFromFile(a)
+
+   presentation.SaveToFile(b, FileFormat.PDF)
+   presentation.Dispose()
+
+
+
+
+
+def main():
+   j=str(sys.argv[1])
+   j2=str(sys.argv[2])
+   if ".pdf" in j and ".docx" in j2:
+      docxtopdf(str(sys.argv[1]),str(sys.argv[2]))
+   elif ".docx" in j and ".pdf" in j2:
+      pdftodocx(j,j2)
+   elif ".ppt" in j and ".pdf" in j2:
+      ptopdf(j,j2)
+
+if __name__="__main__":
+    main()
+
