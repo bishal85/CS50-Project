b062f19d3837fb92039242ef1e4a7cecfc9d4c14
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 07:12:58 +0000

    Tue, Jan 9, 2024, 1:12 PM +06:00

--- shirt/new2.py
+++ shirt/new2.py
@@ -1,8 +1,12 @@
 from spire.doc import *
 from spire.doc.common import *
 
+import sys
 from spire.pdf.common import *
 from spire.pdf import *
+
+from spire.presentation import *
+from spire.presentation.common import *
 def docxtopdf(a,b):
 # Create a Document object
    document = Document()
@@ -15,3 +19,25 @@ def pdftodocx(a,b):
    pdf.LoadFromFile(a)
    pdf.SaveToFile(b, FileFormat.DOCX)
    pdf.Close()
+def ptopdf(a,b):
+   presentation = Presentation()
+# Load a PowerPoint presentation in PPTX format
+   presentation.LoadFromFile(a)
+   presentation.SaveToFile(b, FileFormat.PDF)
+   presentation.Dispose()
+
+
+
+def main():
+   j=str(sys.argv[1])
+   j2=str(sys.argv[2])
+   if ".pdf" in j and ".docx" in j2:
+      docxtopdf(j,j2)
+   elif ".docx" in j and ".pdf" in j2:
+      pdftodocx(j,j2)
+   elif ".ppt" in j and ".pdf" in j2:
+      ptopdf(j,j2)
+
+if __name__="__main__":
+    main()
+
