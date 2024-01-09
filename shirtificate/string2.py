1fb762d37f52fcd4809dfcf7ec2581faec8b0f22
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 08:11:42 +0000

    Tue, Jan 9, 2024, 2:11 PM +06:00

--- shirtificate/string2.py
+++ shirtificate/string2.py
@@ -1,28 +1,16 @@
-from spire.doc import *
-from spire.doc.common import *
-
+from docx2pdf import convert
 import sys
 from spire.pdf.common import *
 from spire.pdf import *
-
 def docxtopdf(a,b):
-# Create a Document object
-   document = Document()
-# Load a Word DOCX file
-   document.LoadFromFile(a)
-   document.SaveToFile(b, FileFormat.PDF)
-   document.Close()
+   convert(a, b)
+
 def pdftodocx(a,b):
    pdf = PdfDocument()
    pdf.LoadFromFile(a)
    pdf.SaveToFile(b, FileFormat.DOCX)
    pdf.Close()
-def ptopdf(a,b):
-   presentation = Presentation()
-# Load a PowerPoint presentation in PPTX format
-   presentation.LoadFromFile(a)
-   presentation.SaveToFile(b, FileFormat.PDF)
-   presentation.Dispose()
+
 
 
 
@@ -33,9 +21,6 @@ def main():
       pdftodocx(j,j2)
    elif ".docx" in j and ".pdf" in j2:
       docxtopdf(j,j2)
-   elif ".ppt" in j and ".pdf" in j2:
-      ptopdf(j,j2)
-
 if __name__=="__main__":
     main()
 
