4bdbccc816e230534e236a90798041f7f35ed63a
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 07:35:40 +0000

    Tue, Jan 9, 2024, 1:35 PM +06:00

--- shirtificate/string5.py
+++ shirtificate/string5.py
@@ -1,12 +1,13 @@
 from spire.pdf.common import *
 from spire.pdf import *
 
+import sys
 # Create a PdfDocument object
 pdf = PdfDocument()
 # Load a PDF file
-pdf.LoadFromFile("new1.pdf")
+pdf.LoadFromFile(str(sys.argv[1]))
 
 # Convert the PDF file to a Word DOCX file
-pdf.SaveToFile("PdfToDocx.docx", FileFormat.DOCX)
+pdf.SaveToFile(str(sys.argv[2]), FileFormat.DOCX)
 # Close the PdfDocument object
 pdf.Close()
