6209bd02bd1157420b01ff9355669a93095d6253
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 08:58:24 +0000

    Tue, Jan 9, 2024, 2:58 PM +06:00

--- shirtificate/string2.py
+++ shirtificate/string2.py
@@ -19,7 +19,12 @@ def pdftodocx(a,b):
 def main():
    j=str(sys.argv[1])
    j2=str(sys.argv[2])
-   docxtopdf(j,j2)
+   if ".pdf" in j and ".docx" in j2:
+      docxtopdf(j,j2)
+   elif ".docx" in j and ".pdf" in j2:
+      pdftodocx(j,j2)
+   elif ".ppt" in j and ".pdf" in j2:
+      ptopdf(j,j2)
 if __name__=="__main__":
     main()
 
