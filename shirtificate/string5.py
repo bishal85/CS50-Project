c0be3e7710565518cd89cecc82df2897e1983d9f
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 07:46:06 +0000

    Tue, Jan 9, 2024, 1:46 PM +06:00

--- shirtificate/string5.py
+++ shirtificate/string5.py
@@ -2,17 +2,9 @@ from spire.pdf.common import *
 from spire.pdf import *
 from spire.doc import *
 from spire.doc.common import *
-from spire.presentation import *
-from spire.presentation.common import *
 import sys
 # Create a PdfDocument object
-def docxtopdf(a1,b1):
-# Create a Document object
-   document = Document()
-# Load a Word DOCX file
-   document.LoadFromFile(a1)
-   document.SaveToFile(b1, FileFormat.PDF)
-   document.Close()
+
 def pdftodocx(a,b):
    pdf = PdfDocument()
    pdf.LoadFromFile(a)
@@ -22,8 +14,6 @@ def main():
    j=str(sys.argv[1])
    j2=str(sys.argv[2])
    if ".pdf" in j and ".docx" in j2:
-      docxtopdf(j,j2)
-   elif ".docx" in j and ".pdf" in j2:
       pdftodocx(j,j2)
 
 
