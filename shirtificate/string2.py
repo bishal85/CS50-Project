fd1003e93d3e18fa5ba61330e05faef763dc1576
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 08:21:42 +0000

    Tue, Jan 9, 2024, 2:21 PM +06:00

--- shirtificate/string2.py
+++ shirtificate/string2.py
@@ -23,10 +23,7 @@ def pdftodocx(a,b):
 def main():
    j=str(sys.argv[1])
    j2=str(sys.argv[2])
-   if ".pdf" in j and ".docx" in j2:
-      pdftodocx(j,j2)
-   elif ".docx" in j and ".pdf" in j2:
-      docxtopdf(j,j2)
+   docxtopdf(j,j2)
 if __name__=="__main__":
     main()
 
