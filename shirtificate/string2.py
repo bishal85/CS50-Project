e4c7efdfce42058072c7f7b8c91b866425540178
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 08:44:29 +0000

    Tue, Jan 9, 2024, 2:44 PM +06:00

--- shirtificate/string2.py
+++ shirtificate/string2.py
@@ -5,10 +5,6 @@ import os.path
 from spire.pdf.common import *
 from spire.pdf import *
 def docxtopdf(a,b):
-   current_working_directory = os.getcwd()
-   print(current_working_directory)
-   path1 =current_working_directory+a
-   path2=current_working_directory+b
    convert(a,b)
 
 def pdftodocx(a,b):
