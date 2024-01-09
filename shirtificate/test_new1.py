0893f1a8c182fad7eea0a6755536c31acfb6de08
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 11:52:56 +0000

    Tue, Jan 9, 2024, 5:52 PM +06:00

--- shirtificate/test_new1.py
+++ shirtificate/test_new1.py
@@ -3,16 +3,22 @@ import os.path
 
 from string2 import docxtopdf
 from string2 import docxtotxt
+
+from string2 import docxtobook
 # get the current working directory
 current_working_directory = os.getcwd()
 
-def test_pdf():
-
+def test_docxtopdf():
     docxtopdf("new1.docx","HELLO1.pdf")
     assert os.path.isfile(current_working_directory+"/HELLO1.pdf")==True
-def test_text():
+def test_docxtotxt():
 
     docxtotxt("new1.docx","HELLO.txt")
     assert os.path.isfile(current_working_directory+"/HELLO.txt")==True
-def test_
+def test_docxtobook():
+
+
+    docxtobook("new1.docx","HELLO.epub")
+    assert os.path.isfile(current_working_directory+"/HELLO.epub")==True
+
 
