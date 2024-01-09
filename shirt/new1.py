6102bfc894a084c1e8cf2555384f66024bf8f0b1
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 06:49:56 +0000

    Tue, Jan 9, 2024, 12:49 PM +06:00

--- shirt/new1.py
+++ shirt/new1.py
@@ -1,7 +1,19 @@
 import os
+import os.path
 
 # get the current working directory
 current_working_directory = os.getcwd()
 
 # print output to the console
 print(current_working_directory)
+
+
+path =current_working_directory
+
+check_file = os.path.isfile(path)
+
+print(check_file)
+
+# output
+
+# True
