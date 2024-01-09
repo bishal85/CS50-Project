f5f94cae8f494e1fcece99c7bbce7d882d644e8a
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 08:16:40 +0000

    Tue, Jan 9, 2024, 2:16 PM +06:00

--- shirt/new1.py
+++ shirt/new1.py
@@ -13,15 +13,7 @@ path ='/workspaces/143246886/shirt'
 check_file = os.path.isfile(path)
 
 print(check_file)
-from pdf2docx import Converter
 
-pdf_file = '/path/to/sample.pdf'
-docx_file = 'path/to/sample.docx'
-
-# convert pdf to docx
-cv = Converter(pdf_file)
-cv.convert(docx_file)      # all pages by default
-cv.close()
 # output
 
 # True
