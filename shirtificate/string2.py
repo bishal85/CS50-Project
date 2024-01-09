1e14ca065b1ee73bc395a4346a84f65642b6d460
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-09 09:44:49 +0000

    Tue, Jan 9, 2024, 3:44 PM +06:00

--- shirtificate/string2.py
+++ shirtificate/string2.py
@@ -7,7 +7,7 @@ from spire.doc.common import *
 def main():
    j=str(sys.argv[1])
    j2=str(sys.argv[2])
-   if ".docx" in j and ".txt" in j2:
+   if ".docx" in j and ".html" in j2:
       docxtotxt(j,j2)
    elif ".docx" in j and ".pdf" in j2:
       docxtopdf(j,j2)
@@ -19,10 +19,10 @@ def docxtopdf(a2,b2):
    document.SaveToFile(b2, FileFormat.PDF)
    document.Close()
 def docxtotxt(a,b):
-   document = Document()
-   document.LoadFromFile(a)
-   document.SaveToFile(a, FileFormat.TXT)
-   document.Close()
+   document1 = Document()
+   document1.LoadFromFile(a)
+   document1.SaveToFile(b, FileFormat.Html)
+   document1.Close()
 def ptopdf(a1,b1):
    print(f"{a1}to{b1}")
 
