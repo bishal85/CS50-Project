7dcb19630947c0ef6fd2fe338d5a79a2dd8146da
bishal85 <143246886+bishal85@users.noreply.github.com>
2024-01-10 09:45:53 +0000

    Wed, Jan 10, 2024, 3:45 PM +06:00

--- project1/project1.py
+++ project1/project1.py
@@ -1,1 +1,39 @@
 
+import sys
+import os
+import os.path
+from spire.doc import *
+from spire.doc.common import *
+def main():
+   j=str(sys.argv[1])
+   j2=str(sys.argv[2])
+   if ".docx" in j and ".txt" in j2:
+      docxtotxt(j,j2)
+   elif ".docx" in j and ".pdf" in j2:
+      docxtopdf(j,j2)
+   elif ".docx" in j and ".epub" in j2:
+      docxtobook(j,j2)
+
+   else:
+      sys.exit("Invalid")
+def docxtopdf(a2,b2):
+    document = Document()
+    document.LoadFromFile(a2)
+    document.SaveToFile(b2, FileFormat.PDF)
+    document.Close()
+def docxtotxt(a,b):
+    document1 = Document()
+    document1.LoadFromFile(a)
+    document1.SaveToFile(b, FileFormat.Txt)
+    document1.Close()
+def docxtobook(a1,b1):
+    document1 = Document()
+    document1.LoadFromFile(a1)
+    document1.SaveToFile(b1, FileFormat.EPub)
+    document1.Close()
+
+
+
+if __name__=="__main__":
+    main()
+
---
+++ project1/test_project.py
@@ -0,0 +1,24 @@
+import os
+import os.path
+
+from project import docxtopdf
+from project import docxtotxt
+
+from orject import docxtobook
+# get the current working directory
+current_working_directory = os.getcwd()
+
+def test_docxtopdf():
+    docxtopdf("new1.docx","HELLO1.pdf")
+    assert os.path.isfile(current_working_directory+"/HELLO1.pdf")==True
+def test_docxtotxt():
+
+    docxtotxt("new1.docx","HELLO.txt")
+    assert os.path.isfile(current_working_directory+"/HELLO.txt")==True
+def test_docxtobook():
+
+
+    docxtobook("new1.docx","HELLO.epub")
+    assert os.path.isfile(current_working_directory+"/HELLO.epub")==True
+
+
